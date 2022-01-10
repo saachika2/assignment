d0195584adb801f228ed12979c4e59d076286cdb
saachika2 <97081246+saachika2@users.noreply.github.com>
2022-01-10 20:31:14 +0530

    Add files via upload

---
+++ answers.py
@@ -0,0 +1,37 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 18:54:59) [MSC v.1929 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: C:\Users\Asus\Downloads\python\assignment.py ============
+Q U E S T I O N  1
+enter number 1:23
+enter number 2:21
+enter number 3:2
+the average of the given numbersis: 15.333333333333334
+Q U E S T I O N  2
+enter gross income:24566
+enter the number of dependents:2
+tax: 1713.2
+Q U E S T I O N  3
+enter the SID:21103781
+enter name:regi
+enter gender F for female, M for male, U for unknown:f
+enter course name:cse
+enter CGPA:10
+SID  NAME GENDER COURSENAME CGPA
+21103781 regi f cse 10.0
+Q U E S T I O N  4
+enter marks of student 1:98
+enter marks of student 2:74
+enter marks of student 3:88
+list:
+[98, 74, 88]
+sorted list in decreasing order:
+[98, 88, 74]
+Q U E S T I O N  5
+given list: ['red', 'green', 'white', 'black', 'pink', 'yellow']
+part a
+new list after removing the fourth element:
+['red', 'green', 'white', 'pink', 'yellow']
+part b
+new list after replacing black and pink with purple:
+['red', 'green', 'white', 'purple', 'purple']
